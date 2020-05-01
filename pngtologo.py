8072d75749809e2ed01a86a616a914ce3edc3228
Eric Roy <royalmo6@gmail.com>
2020-05-01 18:54:25 +0200

    Made it to work

--- pngtologo.py
+++ pngtologo.py
@@ -5,13 +5,25 @@ from PIL import Image
 
 input_file = "test.png"
 output_file = "test.txt"
+multiplier = 2.5
 
 im = Image.open(input_file, "r")
 
 pix_val = list(im.getdata())
 size = im.size
+print(size)
 im.close()
-for pixel in range(20):
-    print (pix_val[pixel])
 
-print (im.size)
+result = "pu setpensize " + str(multiplier) + " fd 250 rt 90 bk 250 pd\n"
+x = 0
+y = 0
+for pixel in pix_val:
+    result += "color [" + str(pixel[0]) + " " + str(pixel[1]) + " " + str(pixel[2]) + "] fd " + str(multiplier) + " "
+    x += 1
+    if x == size[0]:
+        x = 0
+        y += 1
+        result += "pu bk " + str(size[0]*multiplier) + " rt 90 fd " + str(multiplier) + " lt 90 pd\n"
+
+with open(output_file, "w") as fileout:
+    fileout.write(result)
