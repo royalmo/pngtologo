9825b4f277025925dae5d31f3b8a81462b94b7a8
Eric Roy <royalmo6@gmail.com>
2020-05-04 11:54:06 +0200

    Fixed some bugs, started optimisation.

--- pngtologo.py
+++ pngtologo.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from PIL import Image
+from pathlib import Path
 
+directory_path = str(Path(__file__).parent.absolute()) + "/"
 input_file = "test.png"
-output_file = "test.txt"
+output_file =  str(input_file.split(".")[0:-1][0]) + ".txt"
 multiplier = 2.5
 
 im = Image.open(input_file, "r")
@@ -14,7 +16,7 @@ size = im.size
 print(size)
 im.close()
 
-result = "pu setpensize " + str(multiplier) + " fd 250 rt 90 bk 250 pd\n"
+result = "pu setpensize " + str(multiplier) + " fd " + str(size[1]*multiplier/2) + " rt 90 bk " + str(size[0]*multiplier/2) + " pd\n"
 x = 0
 y = 0
 for pixel in pix_val:
