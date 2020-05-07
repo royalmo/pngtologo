3030fd7b58ee247bd78a5073b3e26f7618afd3cd
Eric Roy <royalmo6@gmail.com>
2020-05-08 00:14:18 +0200

    Minor changes

--- runlogo.py
+++ runlogo.py
@@ -3,9 +3,9 @@ This script will take a .txt or maybe a .logo file and will
 return a png picture of the result of running that script.
 I am making my own language!
 """
-# class turtle - direction, position, pupd
-# class pixelgrid - size, fill
-
+from PIL import Image, ImageTk
+import numpy as np
+import tkinter as tk
 
 class LogoGrid():
     def __init__(self, sizeX, sizeY, color = [255, 255, 255]):
@@ -20,7 +20,32 @@ class LogoGrid():
                 line.append(color)
                 x += 1
             self.grid.append(line)
+            line = []
             y += 1
             x = 0
-        
+    def save(self, file):
+        array = np.array(grid.grid, dtype=np.uint8)
+        image = Image.fromarray(array)
+        image.save(file)
+    def getimage(self):
+        array = np.array(grid.grid, dtype=np.uint8)
+        image = Image.fromarray(array)
+        return ImageTk.PhotoImage(image)
+
+class LogoTurtle():
+    def __init__(self, position = [0, 0, 0], facing = 90, pendown = 1, visible = 0):
+        self.position = position
+        self.facing = facing
+        self.pendown = pendown
+        self.visible = visible
+
+grid = LogoGrid(500, 500)
+grid.save('test.png')
+
+root = tk.Tk()
+img = grid.getimage()
+panel = tk.Label(root, image = img)
+panel.pack(side = "bottom", fill = "both", expand = "yes")
+root.mainloop()
+
 
